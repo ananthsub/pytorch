d6e49e867be29876d74c71013c14258e22a8a2da
eellison <eellison@fb.com>
2022-01-11 12:05:05 -0800

    Remove precise tuple construct flag
    ghstack-source-id: a8c64f8e04d4bcd93e04f35881fdb26f524790b1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71121

--- torch/csrc/jit/ir/alias_analysis.cpp
+++ torch/csrc/jit/ir/alias_analysis.cpp
@@ -209,11 +209,9 @@ struct AliasDb::WriteRegistry {
 
 AliasDb::AliasDb(
     std::shared_ptr<Graph> graph,
-    bool isFrozen,
-    bool enablePreciseTupleContainerAnalysis)
+    bool isFrozen)
     : graph_(std::move(graph)),
       isFrozen_(isFrozen),
-      enablePreciseTupleContainerAnalysis_(enablePreciseTupleContainerAnalysis),
       memoryDAGBuilder_(std::make_unique<MemoryDAGBuilder>()),
       writeRegistry_(std::make_unique<AliasDb::WriteRegistry>()) {
   analyze(graph_);
@@ -1146,9 +1144,6 @@ bool AliasDb::functionalNonEscapingListUse(const Use& use) const {
 }
 
 bool AliasDb::functionalNonEscapingTupleUse(const Use& use) const {
-  if (!enablePreciseTupleContainerAnalysis_) {
-    return false;
-  }
   Node* n = use.user;
   size_t offset = use.offset;
   Value* container = n->inputs().at(offset);
@@ -1156,7 +1151,9 @@ bool AliasDb::functionalNonEscapingTupleUse(const Use& use) const {
     return false;
   }
   // TODO(T97387453): Cover more ops that do not let escape tuples' elements.
-  return use.user->kind() == prim::Return;
+  bool in_return_outputs = use.user->kind() == prim::Return;
+  bool not_in_nested_subgraph = use.user->owningBlock() == graph_->block();
+  return in_return_outputs && not_in_nested_subgraph;
 }
 
 // List or dict or tuple construct: create an aliasing element for the actual
